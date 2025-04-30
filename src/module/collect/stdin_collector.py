ef28de360e1b059299199f12a5f3e929943280c8
Michael Lustenberger <mic@inofix.ch>
2025-05-01 01:21:31 +0200

    Example Module
    Add a new example module.

---
+++ src/module/collect/stdin_collector.py
@@ -0,0 +1,31 @@
+# import logging
+
+import sys
+
+from bridging_hub_module import CollectorBaseModule
+
+
+class StdinCollector(CollectorBaseModule):
+    """
+    Read from stdin and return the result.
+    """
+
+    def __init__(self):
+        self._element = None
+
+    def collect(self) -> dict[str, dict[str, str]]:
+        r: dict[str, dict[str, str]] = {}
+        d = list(self._data["value_register_map"])
+        try:
+            # use only as many input lines as there are
+            # data points defined in the config..
+            for line in sys.stdin:
+                k = d.pop()
+                r[k] = {}
+                n = self.current_timestamp()
+                r[k][self._custom_name["timestamp_name"]] = n
+                r[k][self._custom_name["value_name"]] = line
+        except IndexError:
+            # latest configured element reached
+            pass
+        return r
