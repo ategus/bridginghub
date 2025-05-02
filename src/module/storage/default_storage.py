10bdb3adce6ea1440c20c7606eaeb8b2c65fea3e
Michael Lustenberger <mic@inofix.ch>
2025-05-03 00:58:55 +0200

    Storage Cache
    Implement write_cache().

--- src/module/storage/default_storage.py
+++ src/module/storage/default_storage.py
@@ -1,4 +1,7 @@
-from bridging_hub_module import StorageBaseModule
+import json
+import os
+
+from bridging_hub_module import BrokenConfigException, StorageBaseModule
 
 
 class DefaultStorageModule(StorageBaseModule):
@@ -6,11 +9,63 @@ class DefaultStorageModule(StorageBaseModule):
     Message content can be cached and archived or broken messages identified.
     """
 
+    KEY_CACHE: str = "cache"
+    KEY_JUNK: str = "junk"
+    KEY_ARCHIVE: str = "archive"
+
+    _cachedir: str = ""
+
+    def test_dir(self, dir_type: str) -> str:
+        """Check for and prepare storage directory.
+        :param dir_type: the kind of directory in question
+        :rtype: bool
+        :return: whether directory was configured
+        :raise: BrokenConfigException"""
+        if dir_type in self._action_detail and self._action_detail[dir_type]:
+            if os.path.isabs(self._action_detail[dir_type]):
+                try:
+                    os.makedirs(
+                        self._action_detail[dir_type],
+                        exist_ok=True,
+                    )
+                    return self._action_detail[dir_type]
+                except Exception as e:
+                    raise BrokenConfigException(
+                        f"""Directory {dir_type} was requested but failed:""",
+                        e,
+                    )
+            else:
+                raise BrokenConfigException(
+                    f"""Please use an absolute path for {dir_type} storage."""
+                )
+        else:
+            return ""
+
     def write_cache(
         self, message: dict[str, dict[str, str]]
     ) -> dict[str, dict[str, str]]:
         """Remember message content between in- and output."""
-        return message
+        d = self.test_dir(DefaultStorageModule.KEY_CACHE)
+        m: dict[str, dict[str, str]] = {}
+        if d:
+            for k, v in message.items():
+                n = os.path.join(
+                    d,
+                    str(
+                        v[
+                            self._custom_name[
+                                StorageBaseModule.KEY_TIMESTAMP_NAME
+                            ]
+                        ]
+                    )
+                    + "_"
+                    + str(k)
+                    + ".json",
+                )
+                with open(n, "w") as f:
+                    json.dump(v, f)
+                m[k] = v
+        return m
 
     def read_cache(self) -> dict[str, dict[str, str]]:
         """Look up message content between in- and output."""
